c7056923a9cb11ea7ba90ab95c21ede7cbc6455f
Hyperiongate <jim@shift-work.com>
2025-08-07 13:30:55 -0700

    Update __init__.py

--- services/__init__.py
+++ services/__init__.py
@@ -1,6 +1,22 @@
 """
-AI Detection Services Package
+AI & Plagiarism Detection Services Package
 """
 
-# This file makes the services directory a Python package
-# It can be empty, but having this comment helps identify its purpose
+# Import all service modules
+from .ai_detector import AIDetector
+from .text_ai_analyzer import TextAIAnalyzer
+from .pattern_analyzer import PatternAnalyzer
+from .perplexity_analyzer import PerplexityAnalyzer
+from .statistical_analyzer import StatisticalAnalyzer
+from .copyleaks_detector import CopyleaksDetector
+from .plagiarism_detector import PlagiarismDetector
+
+__all__ = [
+    'AIDetector',
+    'TextAIAnalyzer',
+    'PatternAnalyzer',
+    'PerplexityAnalyzer',
+    'StatisticalAnalyzer',
+    'CopyleaksDetector',
+    'PlagiarismDetector'
+]
